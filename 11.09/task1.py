5eb25dab6cf521a575312e5ce6fc93954e67dbea
deepu9789 <144676337+deepu9789@users.noreply.github.com>
2023-09-12 11:09:22 +0530

    Add files via upload

---
+++ 11.09/task1.py
@@ -0,0 +1,12 @@
+Name=("Deepana Periyasamy")
+Address=("West kattu valavu Ariyagoundampatty-637406")
+Class=("Msc computer science")
+T=80
+E=70
+M=80
+Phy=90
+Che=85
+Bio=90
+Total=T+E+M+Phy+Che+Bio
+Avg=Total/6
+print("I'm",Name,"My address",Address,"i'm doing",Class,"My 12th mark:",T,E,M,Phy,Che,Bio,"My total mark",Total,"Avg",Avg)
